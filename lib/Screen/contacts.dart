a768a732f37c33509e7e4cbcb87dfcb0574bea0d
SaiSuvamPatnaik <saisuvam.patnaik1414@gmail.com>
2021-02-23 22:03:00 +0530

    Swipe to delete

--- lib/Screen/contacts.dart
+++ lib/Screen/contacts.dart
@@ -23,73 +23,85 @@ class _contactsState extends State<contacts> {
     ref1 = FirebaseDatabase.instance.reference().child("Contact");
   }
 
-  Widget buildcontactitems({Map contact}){
+  Widget buildcontactitems({Map contact, int pos}){
 
-      return Padding(
-
-
-        padding: const EdgeInsets.fromLTRB(15,10,15,0),
-        child: Container(
-          height: 150,
-          color: Colors.white,
-
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  SizedBox(width: 20,),
-                  Icon(Icons.person,color: Colors.blueAccent,size: 30,),
-                  SizedBox(width: 10,),
-                  Text(contact["Name"],style: TextStyle(fontSize: 20,color: Colors.redAccent,fontWeight: FontWeight.w600),)
-                ],
-              ),
-              SizedBox(height: 10,),
-              Row(
-                children: [
-                  SizedBox(width: 20,),
-                  Icon(Icons.phone_android_rounded,color: Colors.blueAccent,size: 30,),
-                  SizedBox(width: 5,),
-                  Text("+91-"+contact["Phone"],style: TextStyle(fontSize: 20,color: Colors.redAccent,fontWeight: FontWeight.w600),),
-                  SizedBox(width: 35,),
-                  Icon(Icons.group_work,color: gettypecolor(contact["Title"]),size: 30,),
-                  SizedBox(width: 5,),
-                  Text(contact["Title"],style: TextStyle(fontSize: 20,color: gettypecolor(contact["Title"]),fontWeight: FontWeight.w600),)
-                ],
-              ),
-              SizedBox(height: 10,),
-
-              Row(
-                children: [
-                  SizedBox(width: 20,),
-                  RaisedButton.icon(onPressed: (){
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Edit(contactkey: contact["key"],)));
-                  },
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    icon: Icon(Icons.edit,size: 20,),
-                    label: Text("Edit",style: TextStyle(fontSize: 15,color: Colors.black),),),
-
-                  SizedBox(width: 15,),
-
-                  RaisedButton.icon(onPressed: (){
-                    ref1.child(contact["key"]).remove();
-
-                      },
-                    icon: Icon(Icons.delete,size: 20,),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    label: Text("Delete",style: TextStyle(fontSize: 15,color: Colors.black),),),
-                ],
-              ),
+      return Dismissible(
+        key: ObjectKey(contact["key"]),
+        onDismissed: (direction){
+          ref1.child(contact["key"]).remove();
 
 
-            ],
+        },
+        direction: DismissDirection.endToStart,
+        background: Container(
+          color: Colors.red,
+        ),
+        child: Padding(
+
+
+          padding: const EdgeInsets.fromLTRB(15,10,15,0),
+          child: Container(
+            height: 150,
+            color: Colors.white,
+
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    SizedBox(width: 20,),
+                    Icon(Icons.person,color: Colors.blueAccent,size: 30,),
+                    SizedBox(width: 10,),
+                    Text(contact["Name"],style: TextStyle(fontSize: 20,color: Colors.redAccent,fontWeight: FontWeight.w600),)
+                  ],
+                ),
+                SizedBox(height: 10,),
+                Row(
+                  children: [
+                    SizedBox(width: 20,),
+                    Icon(Icons.phone_android_rounded,color: Colors.blueAccent,size: 30,),
+                    SizedBox(width: 5,),
+                    Text("+91-"+contact["Phone"],style: TextStyle(fontSize: 20,color: Colors.redAccent,fontWeight: FontWeight.w600),),
+                    SizedBox(width: 35,),
+                    Icon(Icons.group_work,color: gettypecolor(contact["Title"]),size: 30,),
+                    SizedBox(width: 5,),
+                    Text(contact["Title"],style: TextStyle(fontSize: 20,color: gettypecolor(contact["Title"]),fontWeight: FontWeight.w600),)
+                  ],
+                ),
+                SizedBox(height: 10,),
+
+                Row(
+                  children: [
+                    SizedBox(width: 20,),
+                    RaisedButton.icon(onPressed: (){
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Edit(contactkey: contact["key"],)));
+                    },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      icon: Icon(Icons.edit,size: 20,),
+                      label: Text("Edit",style: TextStyle(fontSize: 15,color: Colors.black),),),
+
+                    SizedBox(width: 15,),
+
+                    RaisedButton.icon(onPressed: (){
+                      ref1.child(contact["key"]).remove();
+
+                        },
+                      icon: Icon(Icons.delete,size: 20,),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      label: Text("Delete",style: TextStyle(fontSize: 15,color: Colors.black),),),
+                  ],
+                ),
+
+
+              ],
+            ),
           ),
         ),
       );
@@ -111,8 +123,9 @@ class _contactsState extends State<contacts> {
         child: FirebaseAnimatedList(query: ref ,itemBuilder: (BuildContext context,
         DataSnapshot snapshot,Animation<double>animation,int index){
           Map contact = snapshot.value;
+          int pos;
           contact["key"] = snapshot.key;
-          return buildcontactitems(contact: contact);
+          return buildcontactitems(contact: contact,pos: index);
         },),
       ),
       floatingActionButton: FloatingActionButton(
